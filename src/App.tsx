579e4c575b032f560f8e8de81536f0b9983605b3
Gabriel Magalhães <gbrmgls@gmail.com>
2024-11-24 22:52:25 -0300

    Debug

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import { Input } from "@/components/ui/input";
 import { createClient } from "@supabase/supabase-js";
 
 import "./App.css";
-console.log(import.meta.env);
-console.log(process.env);
 const supabase = createClient(
   import.meta.env.VITE_DATABASE_URL,
   import.meta.env.VITE_DATABASE_KEY
